301d40ededd21faaa36a1862f296ca24d9066518
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-27 18:02:17 +0200

    core features

--- redux-demo.js
+++ redux-demo.js
@@ -1,18 +1,32 @@
-const redux = reqired("redux");
-
+const redux = require("redux");
 
 //Always should have two parameters as state and action
-const counterReducer = (state, action) => {
- //Should always return new state   
- //will replace the existing state
-  return {
-    counter: state.counter + 1,
-  };
+const counterReducer = (state = { counter: 0 }, action) => {
+  //Should always return new state
+  //will replace the existing state
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    }
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter -1,
+    }
+  }
+  return state
 };
 
 const store = redux.createStore(counterReducer);
+
+// console.log(store.getState())
 
 const counterSubscriber = () => {
   const latestState = store.getState();
   console.log(latestState);
 };
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
